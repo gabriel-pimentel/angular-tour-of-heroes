705e7f496bb599ec2bf1102818f0eb6a57608759
Gabriel Pimentel <gabriel97pimentel@gmail.com>
2021-03-06 11:26:28 -0300

    Adding Heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'John Frusciante'},
+  {id: 12, name: 'Alex Lifeson'},
+  {id: 13, name: 'Guthrie Govan'},
+  {id: 14, name: 'Mario Camarena'},
+  {id: 15, name: 'Cacau Santos'},
+  {id: 16, name: 'Neil Rogers'},
+  {id:17, name: 'Layney Stayley'},
+  {id: 18, name: 'Mano Brown'},
+  {id: 19, name: 'Kali Uchis'},
+  {id: 20, name: 'Lady Gaga'}
+];
+
